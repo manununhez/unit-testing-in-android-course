987a2e3562b51addb06b0b2ab20d30c7bd9964f6
Manuel Nunez <03.manu@gmail.com>
2019-08-31 23:53:42 +0200

    Completed Exercise 2!

--- unit_testing_fundamentals/src/test/java/com/techyourchance/unittestingfundamentals/exercise2/StringDuplicatorTest.java
+++ unit_testing_fundamentals/src/test/java/com/techyourchance/unittestingfundamentals/exercise2/StringDuplicatorTest.java
@@ -1,5 +1,34 @@
 package com.techyourchance.unittestingfundamentals.exercise2;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
 public class StringDuplicatorTest {
+    StringDuplicator SUT;
+    @Before
+    public void setUp() throws Exception {
+        SUT = new StringDuplicator();
+    }
+
+    @Test
+    public void duplicate_emptyString_emptyStringReturned() throws Exception {
+        String result = SUT.duplicate("");
+        assertThat(result, is(""));
+    }
+
+    @Test
+    public void duplicate_singleCharacter_duplicateCharacterReturned() throws Exception {
+        String result = SUT.duplicate("a");
+        assertThat(result, is("aa"));
+    }
+
+    @Test
+    public void duplicate_longString_duplicateStringReturned() throws Exception {
+        String result = SUT.duplicate("Manuel Nunez");
+        assertThat(result, is("Manuel NunezManuel Nunez"));
+    }
 
 }
